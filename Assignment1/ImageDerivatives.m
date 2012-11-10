dc4828b4fc04b221618d59909ba17e6f88801c64
robrechtjurriaans <robrechtjurriaans@gmail.com>
2012-11-10 16:21:17 +0100

    is this correct then?

--- Assignment1/ImageDerivatives.m
+++ Assignment1/ImageDerivatives.m
@@ -1,22 +1,30 @@
 function F= ImageDerivatives ( img , sigma , type )
    halfSize = floor(1.5 * sigma);
    x = -halfSize:halfSize;
-   
    G = gaussian(sigma);  
-   kernel = -(((x.^2) - (sigma^2) )./ (sigma^4)) .* G;
+   
+switch type
+    case {'x', 'y', 'xy','yx'}
+        Gd = gaussianDer(G, sigma);
+        
+    case {'xx', 'yy'}
+        Gdd = ( ( -(sigma^2) + (x.^2) ) ./ (sigma^4) ) .* G;
+        plot(Gdd)
+        figure
+end
 
 switch type
     case 'x'
-        F = conv2(img, kernel, 'same');
+        F = conv2(img, Gd, 'same');
     case 'y'
-        F = conv2(img', kernel, 'same')';
+        F = conv2(img', Gd, 'same')';
     case 'xx'
-        F = conv2(conv2(img, kernel, 'same'),kernel, 'same');
-    case 'xy'
-        F = conv2(conv2(img, kernel, 'same')',kernel, 'same')';
-    case 'yx'
-        F = conv2(conv2(img', kernel, 'same')',kernel, 'same');
+        F = conv2(img, Gdd, 'same');
+    case {'xy', 'yx'}
+        F = conv2(Gd, Gd, img, 'same');
     case 'yy'
-        F = conv2(conv2(img', kernel, 'same'),kernel, 'same')';
-
+        F = conv2(img', Gdd, 'same')';
+    otherwise
+        error('Unknown type: type must be in {x, y, xx, xy, yx, yy}');
+end
 end
