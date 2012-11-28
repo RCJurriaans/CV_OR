840413ce8f5023e5a3e18cc441ace40904e4f250
Taco <taco.cohen@gmail.com>
2012-11-28 14:06:32 +0100

    .

---
+++ Assignment4/readimages.m
@@ -0,0 +1,4 @@
+sphere1 = rgb2gray(im2double(imread('sphere1.ppm')));
+sphere2 = rgb2gray(im2double(imread('sphere2.ppm')));
+synth1 = im2double(imread('synth1.pgm'));
+synth2 = im2double(imread('synth2.pgm'));
