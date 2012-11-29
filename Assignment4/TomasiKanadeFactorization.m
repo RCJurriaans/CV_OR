3cb9aaeecb1c2ff5ea28008a189a72c85899bfce
Taco <taco.cohen@gmail.com>
2012-11-29 17:01:51 +0100

    .

--- Assignment4/TomasiKanadeFactorization.m
+++ Assignment4/TomasiKanadeFactorization.m
@@ -1,25 +1,39 @@
-function [M, S] = TomasiKanadeFactorization( D )
+function [M, S] = TomasiKanadeFactorization( D, colors )
 %
 
-    % 
+    % Decompose measurement matrix
     [U,W,V] = svd(D);
     
     sqrtW3 = sqrt(W(1:3,1:3));
-    M = U(:,1:3) * sqrtW;
-    S = sqrtW * V(:,1:3).T;
+    M = U(:,1:3) * sqrtW3;
+    S = sqrtW3 * V(:,1:3)';
     
     % Deal with affine ambiguity
-    L = inv( tranpose(M) * M );
+    L = inv( M' * M );
     C = chol(L)';
     
+    % Update Motion and Structure matrices
     M = M * C;
-    S = inv(C) * M;
+    S = inv(C) * S;
     
+    % Plot points
+    size(S)
     X = S(1,:)';
     Y = S(2,:)';
     Z = S(3,:)';
-    scatter3(X,Y,Z,20, [1 0 0])
+    scatter3(X,Y,Z,20, colors, 'filled')
     
-    
+%    L
+%    C
+%    M' * M
+%    
+%    M' * M
+%    
+%    figure 
+%    X = S(1,:)';
+%    Y = S(2,:)';
+%    Z = S(3,:)';
+%    scatter3(X,Y,Z,20, colors, 'filled')
+   
 end
 
--- Assignment4/readMeasurementMatrix.m
+++ Assignment4/readMeasurementMatrix.m
@@ -1,3 +1,15 @@
-function M = readMeasurementMatrix()
+function [M, MC] = readMeasurementMatrix( )
     M = textread('model house/measurement_matrix.txt');
+       
+    numPts = size(M,1) / 2;
+    numCams = size(M,2);
+        
+    xMask = repmat( [1 0]', numPts, numCams );
+    yMask = repmat( [0 1]', numPts, numCams );
+        
+    xAvgs = sum(xMask .* M) / numPts;
+    yAvgs = sum(yMask .* M) / numPts;
+       
+    MC = M - (repmat(xAvgs, numPts*2, 1) .* xMask) - (repmat(yAvgs, numPts*2, 1) .* yMask);
+    
 end
