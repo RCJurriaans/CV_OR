538b71c794a0a251402be0fd76dd9f41b352070e
robrechtjurriaans <robrechtjurriaans@gmail.com>
2012-12-03 21:30:34 +0100

    some minor changes, should be working now, but F looks weird

--- Assignment5/estimateFundamental.m
+++ Assignment5/estimateFundamental.m
@@ -1,82 +1,81 @@
 function [bestF bestinliers] = estimateFundamental(match1, match2)
-F = zeros(9,1);
+
 bestcount = 0;
 bestinliers = [];
 
 iterations = 50;
 % We need a better value for this
-threshold  = 0.01;
+threshold  = 0.001;
 P=8;
 
-for i=1:iterations
+i=0;
+while i<iterations
     % Take initial seed
     perm = randperm(size(match1,2));
     seed = perm(1:P);
     
     % Estimate F
-    % Estimate A
     A = getA(match1(:,seed), match2(:,seed));
     [~,~,Vt] = svd(A);
     F = Vt(:,size(Vt,2));
-    F = reshape(F,3,3);
-    F = singularizeF(F);
+    F = reshape(F,3,3);  
+    
     % Find inliers using Sampson distance
     numer = (diag(match2' * (F*match1))').^2;
     Fm1 = F*match1;
-    Fm2 = F*match2;
+    Fm2 = F'*match2;
     denom = sum([Fm1(1:2,:);Fm2(1:2,:)].^2);
     sd = numer./denom;
     seed = find(sd<threshold);
-    
-    %    for pc=1:size(match1,2)
-    %       numer = (match2(:,pc)'*(F*match1(:,pc))).^2;
-    %       Fm1 = F*match1(:,pc);
-    %       Fm2 = F*match2(:,pc);
-    %       denom = Fm1(1)^2+Fm1(2)^2+Fm2(1)^2+Fm2(2)^2;
-    %       sd(pc) = numer/denom;
-    %    end
-    %    seed = find(sd<threshold);
     
     % Use inliers to re-estimate F
     A = getA(match1(:,seed), match2(:,seed));
     [~,~,Vt] = svd(A);
     F = Vt(:,size(Vt,2));
     F = reshape(F,3,3);
-    F = singularizeF(F);
+    
     % Find inliers
     numer = (diag(match2' * (F*match1))').^2;
     Fm1 = F*match1;
-    Fm2 = F*match2;
+    Fm2 = F'*match2;
     denom = sum([Fm1(1:2,:);Fm2(1:2,:)].^2);
     sd = numer./denom;
     inliers = find(sd<threshold);
     
-    %    for pc=1:size(match1,2)
-    %       numer = (match2(:,pc)'*(F*match1(:,pc))).^2;
-    %       Fm1 = F*match1(:,pc);
-    %       Fm2 = F*match2(:,pc);
-    %       denom = Fm1(1)^2+Fm1(2)^2+Fm2(1)^2+Fm2(2)^2;
-    %       sd(pc) = numer/denom;
-    %    end
-    %    inliers = find(sd<threshold);
-    
     % if inlier count< best sofar, use new F
     if size(inliers,2)>bestcount
         bestcount=size(inliers,2);
-        bestF = reshape(F,3,3);
+        bestF = F;
         bestinliers=inliers;
     end
+    
+    % Within a chance of epsilon, find number of required iterations
+    % N1 is the largest set on inliers so far
+    % N is the total number of data-points = size(match1,2)
+    % k is the number of data-points that are needed
+    eps = 0.001;
+    N1 = bestcount;
+    N = size(match1,2);
+    k=P;
+    q = (N1/N)^k;
+    iterations = ceil( log(eps)/log(1-q));
+    
+    i = i+1;
+    
 end
+
+disp(strcat(int2str(iterations), ' iterations used to estimate F'));
+
 end
 
 function A = getA(match1, match2)
 A = [match1(1,:).*match2(1,:);...
-    match1(1,:).*match2(2,:);...
-    match1(1,:)             ;...
-    match1(2,:).*match2(1,:);...
-    match1(2,:).*match2(2,:);...
-    match1(2,:)             ;...
-    match2(1,:);...
-    match2(2,:);...
-    ones(1,size(match1,2))     ]';
+     match1(1,:).*match2(2,:);...
+     match1(1,:)             ;...
+     match1(2,:).*match2(1,:);...
+     match1(2,:).*match2(2,:);...
+     match1(2,:)             ;...
+     match2(1,:);...
+     match2(2,:);...
+     ones(1,size(match1,2))     ]';
 end
--- Assignment5/normalize.m
+++ Assignment5/normalize.m
@@ -1,10 +1,14 @@
 function [Pout, T] = normalize( Pin )
+% Center the points
+means = mean(Pin(1:2,:), 2);
+Pzm = Pin - repmat(means, 1, size(Pin, 2));
 
-    means = mean(Pin(1:2,:), 2);    
-    Pzm = Pin - repmat(mean(Pin, 2), 1, size(Pin, 2));
-    d = sum(sqrt(sum(Pzm .^ 2, 1))) / size(Pin, 2);
-    s = sqrt(2) / d;
-    T = [s 0 0; 0 s 0; 0 0 1] * [1 0 -mean(1); 0 1 -mean(2); 0 0 1];
-    Pout = T * [Pin; ones(1,size(Pin,2))];
-    
+% Compute the scaling
+d = sum(sqrt(sum(Pzm .^ 2, 1))) / size(Pin, 2);
+s = sqrt(2) / d;
+
+% Compute the transformation matrix, and transform the inputs
+T = [s 0 0; 0 s 0; 0 0 1] * [1 0 -means(1); 0 1 -means(2); 0 0 1];
+Pout = T * [Pin; ones(1,size(Pin,2))];
+
 end
