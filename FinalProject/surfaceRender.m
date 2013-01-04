27b971d1c2abda999ce577da8016cc5f8a00e7f5
robrechtjurriaans <robrechtjurriaans@gmail.com>
2013-01-04 11:20:11 +0100

    Added color surface render

--- FinalProject/surfaceRender.m
+++ FinalProject/surfaceRender.m
@@ -6,7 +6,7 @@ X = pointcloud(1,:)';
 Y = pointcloud(2,:)';
 Z = pointcloud(3,:)';
 
-ti = -300:300;
+ti = -400:400;
 [qx,qy] = meshgrid(ti,ti);
 
 % Surface generation
@@ -30,9 +30,12 @@ xi2(isnan(yi2))=1;
 yi2(isnan(yi2))=1;
 
 if(size(img,3)==3)
-    Cr = diag(img(floor(yi2), floor(xi2),1));
-    Cg = diag(img(floor(yi2), floor(xi2),2));
-    Cb = diag(img(floor(yi2), floor(xi2),3));
+    imgr = img(:,:,1);
+    imgg = img(:,:,2);
+    imgb = img(:,:,3);
+    Cr = imgr(sub2ind(size(imgr), round(yi2), round(xi2)));
+    Cg = imgg(sub2ind(size(imgg), round(yi2), round(xi2)));
+    Cb = imgb(sub2ind(size(imgb), round(yi2), round(xi2)));
     qc(:,:,1) = reshape(Cr,size(qx));
     qc(:,:,2) = reshape(Cr,size(qy));
     qc(:,:,3) = reshape(Cr,size(qz));
@@ -44,14 +47,14 @@ end
 
 surf(qx, qy, qz, qc);
 
+camlight right
+
 % Render parameters
-axis( [-300 300 -300 300 -300 300] );
+axis( [-500 500 -500 500 -500 500] );
 daspect([1 1 1]);
 rotate3d;
-%shading interp;
+
 shading flat;
-%camlight left;
-%lighting phong;
 
 
 end
